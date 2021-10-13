db212f809352696acc18e03e37be5e4231e742cc
borisbolsh <bolsh.bo@yandex.ru>
2021-10-13 17:23:35 +0300

    project 10

---
+++ P10_NamesToFaces/P10_NamesToFaces/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  P10_NamesToFaces
+//
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ P10_NamesToFaces/P10_NamesToFaces/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  P10_NamesToFaces
+//
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+}
