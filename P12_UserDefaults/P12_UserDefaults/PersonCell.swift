74f7f0c22adf65513aa0ff514997c074eb531ed6
borisbolsh <bolsh.bo@yandex.ru>
2021-10-14 15:50:46 +0300

    12 project

---
+++ P12_UserDefaults/P12_UserDefaults/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  P12_UserDefaults
+//
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+}
