c69c3988776d482472a48e8a26014856ccadebe1
borisbolsh <bolsh.bo@yandex.ru>
2021-10-15 23:44:29 +0300

    Project 28

---
+++ P28_SecretSwift/P28_SecretSwift/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  P28_SecretSwift
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var secret: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func authenticateTapped(_ sender: UIButton) {
+    }
+}
+
